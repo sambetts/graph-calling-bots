f41fc049a9e95553241bba6e86f135f1a451d7ff
Sam Betts <sambetts@microsoft.com>
2023-07-10 17:42:52 +0200

    ChatBotWithCalling add

---
+++ src/examples/ChatBotWithCalling/Solution/Bot/Controllers/ResumeTeamsConversationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace Bot.Controllers;
+
+/// <summary>
+/// Just to send a follow-up to an existing conversation
+/// </summary>
+[Route("api/ResumeTeamsConversation")]
+public class ResumeTeamsConversationController : ControllerBase
+{
+    private readonly IBotFrameworkHttpAdapter _adapter;
+    private readonly Config _config;
+    private readonly IBotConversationCache _botConversationCache;
+
+    public ResumeTeamsConversationController(IBotFrameworkHttpAdapter adapter, Config config, IBotConversationCache botConversationCache)
+    {
+        this._adapter = adapter;
+        this._config = config;
+        this._botConversationCache = botConversationCache;
+    }
+
+    // POST: api/ResumeTeamsConversation?userId=48fe59a4-c951-43ca-9d16-972083aa6305&message=Hi there
+    [HttpPost]
+    public async Task<IActionResult> ResumeTeamsConversation(string userId, string message)
+    {
+        if (string.IsNullOrEmpty(message))
+        {
+            return BadRequest("No message to send");
+        }
+
+        var userCache = _botConversationCache.GetCachedUser(userId);
+        if (userCache != null)
+        {
+            var previousConversationReference = new ConversationReference()
+            {
+                ChannelId = userCache.ChannelId,
+                ServiceUrl = userCache.ServiceUrl,
+                Conversation = new ConversationAccount() { Id = userCache.ConversationId },
+            };
+
+            var cancellationToken = CancellationToken.None;
+
+            // Ping an update using previous conversation reference (cached conversation ID)
+            await ((BotAdapter)_adapter).ContinueConversationAsync(_config.MicrosoftAppId, previousConversationReference,
+                async (turnContext, cancellationToken)
+                    => await turnContext.SendActivityAsync(message, cancellationToken: cancellationToken), cancellationToken);
+
+            return Ok();
+        }
+        return NotFound("User has no cached threads");
+    }
+}
---
+++ src/examples/ChatBotWithCalling/Solution/Bot/TeamsBot.cs
@@ -0,0 +1,34 @@
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bot;
+
+public class TeamsBot : ActivityHandler
+{
+
+    public TeamsBot()
+    {
+    }
+
+    protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+    {
+        var replyText = $"Echo: {turnContext.Activity.Text}";
+        await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+    }
+
+    protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+    {
+        var welcomeText = "Hello and welcome!";
+        foreach (var member in membersAdded)
+        {
+            if (member.Id != turnContext.Activity.Recipient.Id)
+            {
+                // Say hi
+                await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+            }
+        }
+    }
+}
